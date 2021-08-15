eaeee3e8d4d5eb6de2b52808721e5d2301de0ff9
Waleed Yousaf <waleedyousaf@wanclouds.net>
2021-08-15 17:59:43 +0500

    adds whole game to git

---
+++ Assets/Scripts/audiomanager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiomanager : MonoBehaviour {
+
+    public GameObject musicSrc;
+    public GameObject soundSrc;
+
+    public GameObject soundOn;
+    public GameObject soundOff;
+    public GameObject musicOff;
+    public GameObject musicOn;
+
+	void Start () {
+        PlayerPrefs.DeleteAll();
+        if (!PlayerPrefs.HasKey("music"))
+        {
+            PlayerPrefs.SetInt("music", 1);
+        }
+        if (!PlayerPrefs.HasKey("sound"))
+        {
+            PlayerPrefs.SetInt("sound", 1);
+        }
+        if (PlayerPrefs.GetInt("music") == 1)
+        {
+            musicSrc.GetComponent<AudioSource>().Play();
+            musicOn.SetActive(true);
+            musicOff.SetActive(false);
+        }
+        else
+        {
+            musicSrc.GetComponent<AudioSource>().Stop();
+            musicOn.SetActive(false);
+            musicOff.SetActive(true);
+        }
+        if (PlayerPrefs.GetInt("sound") == 1)
+        {
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+        }
+        else
+        {
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+        }
+    }
+
+    public void onMusicTurnOn() {
+        musicOn.SetActive(true);
+        musicOff.SetActive(false);
+        PlayerPrefs.SetInt("music", 1);
+        musicSrc.GetComponent<AudioSource>().Play();
+        if (PlayerPrefs.GetInt("sound") == 1) {
+            soundSrc.GetComponent<AudioSource>().Play();
+        }
+    }
+    public void onMusicTurnOff() {
+        musicOn.SetActive(false);
+        musicOff.SetActive(true);
+        PlayerPrefs.SetInt("music", 0);
+        musicSrc.GetComponent<AudioSource>().Stop();
+        if (PlayerPrefs.GetInt("sound") == 1)
+        {
+            soundSrc.GetComponent<AudioSource>().Play();
+        }
+    }
+    public void onSoundTurnOn() {
+        PlayerPrefs.SetInt("sound", 1);
+        soundOn.SetActive(true);
+        soundOff.SetActive(false);
+        soundSrc.GetComponent<AudioSource>().Play();
+    }
+    public void onSoundTurnOff() {
+        PlayerPrefs.SetInt("sound", 0);
+        soundOn.SetActive(false);
+        soundOff.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/btnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnManager : MonoBehaviour {
+    public GameObject helpPanel;
+    public GameObject leftBtns;
+    public GameObject helpBtn;
+    public KeyCode back;
+    public void Update() {
+        if (Input.GetKeyDown(back)) {
+            helpPanel.GetComponent<Animator>().SetBool("helpinCheck", false);
+            leftBtns.GetComponent<Animator>().SetBool("leftOutCheck", false);
+            helpBtn.GetComponent<Animator>().SetBool("helpBtnOutCheck", false);
+        }
+    }
+    public void playBtn() {
+        Application.LoadLevel(1);
+    }
+    public void SettingsBtn() {
+        Application.LoadLevel(2);
+    }
+    public void StoreBtn() {
+        Application.LoadLevel(3);
+    }
+    public void HelpBtn() {
+        helpPanel.GetComponent<Animator>().SetBool("helpinCheck", true);
+        leftBtns.GetComponent<Animator>().SetBool("leftOutCheck",true);
+        helpBtn.GetComponent<Animator>().SetBool("helpBtnOutCheck", true);
+    }
+}
